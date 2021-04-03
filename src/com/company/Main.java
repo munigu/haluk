9301edb8625c838294ca231732448f40af0eb918
ibrahim munigu <51775496+munigu@users.noreply.github.com>
2021-04-03 00:06:54 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String name = "haluk";
-        String nam1 = "memo";
+        String name = "memo";
+        String nam1 = "haluk";
     }
 }
